f2eddfcf0e6a8196d4ad9a09cc01e74848d3ba87
Dawson <dawsonzhang2007@gmail.com>
2022-12-09 20:06:08 -0600

    added welcome page

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Welcome.css'
+
+export default function Welcome () {
+  return (
+    <div className="WelcomeText">
+      <div className='WelcomeText-big'>
+        <p>Welcome to <em>this</em> grade calculator</p>
+      </div>
+      <div className='WelcomeText-small'>
+        <p>I really don't know why I made this</p>
+      </div>
+    </div>
+  );
+}
