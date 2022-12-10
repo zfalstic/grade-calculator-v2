57726398cba48b93932bb0bcc711e3b37e299dbc
Dawson Zhang <dawsonzhang2007@gmail.com>
2022-12-10 17:26:16 -0600

    lowkey scuffed but also highkey working navigation setup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
 import './Home.css'
 
 export default function Home() {
   return (
-    <div className='WelcomeText'>
-      <div className='WelcomeText-big'>
-        <p>Welcome to <em>this</em> grade calculator</p>
+    <div>
+      <div className='WelcomeText'>
+        <div className='WelcomeText-big'>
+          <p>Welcome to <em>this</em> grade calculator</p>
+        </div>
+        <div className='WelcomeText-small'>
+          <p>I really don't know why I made this</p>
+        </div>
       </div>
-      <div className='WelcomeText-small'>
-        <p>I really don't know why I made this</p>
+      <div className='NavContainer'>
+        <div className='NavText'>
+          <p>If you want to calculate your average in a class by entering each grade</p>
+          <p>If you want to calculate what you need to make on your final to hit a target grade</p>
+        </div>
+        <div className='NavButton'>
+          <Link to='/grade-calculator-v2/average'><button>Class Average Calculator</button></Link>
+          <Link to='/grade-calculator-v2/final'><button>Final Calculator</button></Link>
+        </div>        
       </div>
     </div>
   );
