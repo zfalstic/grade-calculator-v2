90d17d286d49ce93bebfc66177d69685b0955cfb
Dawson <dawsonzhang2007@gmail.com>
2022-12-12 13:35:59 -0600

    refactoring html and css :)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,22 +6,18 @@ import './Home.css'
 export default function Home() {
   return (
     <div>
-      <div className='WelcomeText'>
-        <div className='WelcomeText-big'>
-          <p>Welcome to <em>this</em> grade calculator</p>
-        </div>
-        <div className='WelcomeText-small'>
-          <p>I really don't know why I made this</p>
-        </div>
+      <div className='welcome'>
+        <p className='welcome__text welcome__text--big welcome__text--black'>Welcome to <em>this</em> grade calculator</p>
+        <p className='welcome__text welcome__text--small welcome__text--gray'>I really don't know why I made this</p>
       </div>
-      <div className='NavContainer'>
-        <div className='NavText'>
-          <p>If you want to calculate your average in a class by entering each grade</p>
-          <p>If you want to calculate what you need to make on your final to hit a target grade</p>
+      <div className='navigation'>
+        <div className='navigation__text-container'>
+          <p className='navigation__text'>If you want to calculate your average in a class by entering each grade</p>
+          <p className='navigation__text'>If you want to calculate what you need to make on your final to hit a target grade</p>
         </div>
-        <div className='NavButton'>
-          <Link to='/grade-calculator-v2/average'><button className='ActualNavButton'>Class Average Calculator</button></Link>
-          <Link to='/grade-calculator-v2/final'><button className='ActualNavButton'>Final Calculator</button></Link>
+        <div className='navigation__button-container'>
+          <Link to='/grade-calculator-v2/average'><button className='navigation__button'>Class Average Calculator</button></Link>
+          <Link to='/grade-calculator-v2/final'><button className='navigation__button'>Final Calculator</button></Link>
         </div>        
       </div>
     </div>
